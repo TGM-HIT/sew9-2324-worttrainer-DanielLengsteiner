c550ffb32fbab15fa5bea1f66a405367df16d81d
DanielLengsteiner <dlengsteiner@student.tgm.ac.at>
2023-12-06 08:29:04 +0100

    enabled build scan

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "at.ac.tgm.dlengsteiner"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass = "at.ac.tgm.dlengsteiner.model.Worttrainer"
+}
